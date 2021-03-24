6ea7751fededda19339e6755098bdff49878abce
DANA <dana.yohann@gmail.com>
2021-03-24 15:38:39 +0100

    Creation d'une liste (du taquin) grace au fichier.txt (attention tout les cas d'erreurs ne s'y sont pas encore

---
+++ taquin.py
@@ -0,0 +1,36 @@
+import sys
+
+class Game:
+    def __init__(self, size, file):
+        self.file = file;
+        self.taquin = self.create_taquin();
+
+    def create_taquin(self):
+        ZONE = "";
+        LIST = [];
+        for line in self.file.readlines():
+            line = line.replace('\n', '');
+            i = line.find('#');
+            if (i >= 0):
+                line = line[0:i];
+            if line:
+                ZONE = ZONE + line + " ";
+        LIST = ZONE.split();
+        self.size = int(LIST[0]);
+        taquin = [[0] * self.size for i in range(self.size)];
+        k = 1;
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                taquin[i][j] = LIST[k];
+                k = k + 1;    
+        return taquin
+
+if (len(sys.argv) < 2):
+    print("Need a file with a n_puzzle inside.");
+    sys.exit(1);
+
+f = open(sys.argv[1], "r");
+game = Game(4, f);
+
+for arg in sys.argv:
+    print(arg);
